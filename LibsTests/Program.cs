204f3c492c110ca621abadde526e16c3ff874fca
Xian55 <polereczkrisz@gmail.com>
2021-03-04 19:49:38 +0100

    LibsTests project - NpcNameFinder test

---
+++ LibsTests/Program.cs
@@ -0,0 +1,28 @@
+﻿using Serilog;
+using Serilog.Extensions.Logging;
+
+namespace LibsTests
+{
+    class Program
+    {
+        private static Microsoft.Extensions.Logging.ILogger logger;
+
+        private static void CreateLogger()
+        {
+            var logConfig = new LoggerConfiguration()
+                .WriteTo.File("names.log")
+                .CreateLogger();
+
+            Log.Logger = logConfig;
+            logger = new SerilogLoggerProvider(Log.Logger).CreateLogger(nameof(Program));
+        }
+
+        static void Main(string[] args)
+        {
+            CreateLogger();
+
+            var test = new Test_NpcNameFinder(logger);
+            test.Execute();
+        }
+    }
+}
