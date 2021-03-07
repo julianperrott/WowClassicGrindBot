375e85c42a0777c43cd0c2490414607c1ba82c57
Xian55 <polereczkrisz@gmail.com>
2021-03-07 08:36:08 +0100

    Add new project called Shared - WoWPoint and WorldMapArea

---
+++ Shared/WorldMapArea.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Shared
+{
+    public class WorldMapArea
+    {
+        public int ID { get; set; }
+        public int MapID { get; set; }
+        public int AreaID { get; set; }
+        public string AreaName { get; set; } = string.Empty;
+        public float LocLeft { get; set; }
+        public float LocRight { get; set; }
+        public float LocTop { get; set; }
+        public float LocBottom { get; set; }
+        public int UIMapId { get; set; }
+        public string Continent { get; set; } = string.Empty;
+    }
+}
