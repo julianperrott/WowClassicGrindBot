727e4b0e7c6d51c10690e47b209fb14d4abe1770
Xian55 <polereczkrisz@gmail.com>
2021-03-23 15:13:22 +0100

    Added Util project: WoWheadDB extractor

---
+++ Utilities/WowheadDB_Extractor/Areas.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WowheadDB_Extractor
+{
+    public static class Areas
+    {
+        public static string GetUrl(string key)
+        {
+            _ = Classic.TryGetValue(key, out int link);
+            return $"https://wow.zamimg.com/images/wow/classic/maps/enus/zoom/{link}.jpg";
+        }
+
+        public static Dictionary<string, int> Classic = new Dictionary<string, int>
+        {
+        // EK
+        { " ---------------------- Eastern Kingdom ----------------------", 0 },
+
+        { "Alterac Mountains", 36 },
+        { "Arathi Highlands", 45 },
+        { "Badlands", 3 },
+        { "Blasted Lands", 4 },
+        { "Burning Steppes", 46 },
+        { "Deadwind Pass", 41 },
+        { "Dun Morogh", 1 },
+        { "Duskwood", 10 },
+        { "Eastern Plaguelands", 139 },
+        { "Elwynn Forest", 12 },
+        { "Hillsbrad Foothills", 267 },
+        { "Ironforge", 1537 },
+        { "Loch Modan", 38 },
+        { "Redridge Mountains", 44 },
+        { "Searing Gorge", 51 },
+        { "Silverpine Forest", 130 },
+        { "Stormwind City", 1519 },
+        { "Stranglethorn Vale", 33 },
+        { "Swamp of Sorrows", 8 },
+        { "The Hinterlands", 47 },
+        { "Tirisfal Glades", 85 },
+        { "Undercity", 1497 },
+        { "Western Plaguelands", 28 },
+        { "Westfall", 40 },
+        { "Wetlands", 11 },
+
+        // Kalimdor
+        { " ---------------------- Kalimdor ----------------------", 0 },
+
+        { "Ashenvale", 331 },
+        { "Azshara", 16 },
+        { "Darkshore", 148 },
+        { "Darnassus", 1657 },
+        { "Desolace", 405 },
+        { "Durotar", 14 },
+        { "Dustwallow Marsh", 15 },
+        { "Felwood", 361 },
+        { "Feralas", 357 },
+        { "Moonglade", 493 },
+        { "Mulgore", 215 },
+        { "Orgrimmar", 1637 },
+        { "Silithus", 1377 },
+        { "Stonetalon Mountains", 406 },
+        { "Tanaris", 440 },
+        { "Teldrassil", 141 },
+        { "The Barrens", 17 },
+        { "Thousand Needles", 400 },
+        { "Thunder Bluff", 1638 },
+        { "Un'Goro Crater", 490 },
+        { "Winterspring", 618 }
+    };
+    }
+}
---
+++ Utilities/WowheadDB_Extractor/Program.cs
@@ -0,0 +1,99 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WowheadDB;
+
+namespace WowheadDB_Extractor
+{
+    class Program
+    {
+        private const string outputPath = "../../../../../Json/area/";
+        private const string ZONE_URL = "https://classic.wowhead.com/zone=";
+
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        async static Task MainAsync(string[] args)
+        {
+            await ExtractZones();
+        }
+
+        static async Task ExtractZones()
+        {
+            foreach (KeyValuePair<string, int> entry in Areas.Classic)
+            {
+                if (entry.Value == 0) continue;
+                try
+                {
+                    var p = GetPayloadFromWebpage(await LoadPage(entry.Value));
+                    var z = ZoneFromJson(p);
+                    SaveZone(z, entry.Value.ToString());
+
+                    Console.WriteLine($"Saved {entry.Value,5}={entry.Key}");
+                }
+                catch { }
+
+                await Task.Delay(50);
+            }
+        }
+
+        static async Task<string> LoadPage(int zoneId)
+        {
+            var url = ZONE_URL + zoneId;
+
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(url);
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        static string GetPayloadFromWebpage(string content)
+        {
+            string beginPat = "<script>var mapShower = new ShowOnMap(";
+            string endPat = ");</script>";
+
+            int beginPos = content.IndexOf(beginPat);
+            int endPos = content.IndexOf(endPat, beginPos);
+
+            return content.Substring(beginPos + beginPat.Length, endPos - beginPos - beginPat.Length);
+        }
+
+        static Area ZoneFromJson(string content)
+        {
+            return JsonConvert.DeserializeObject<Area>(content);
+        }
+
+        static void SaveZone(Area zone, string name)
+        {
+            var output = JsonConvert.SerializeObject(zone);
+            var file = Path.Join(outputPath, name  + ".json");
+
+            File.WriteAllText(file, output);
+        }
+
+
+        #region local tests
+
+        static void SerializeTest()
+        {
+            int zoneId = 40;
+            var file = Path.Join(outputPath, zoneId + ".json");
+            var zone = ZoneFromJson(File.ReadAllText(file));
+        }
+
+        static void ExtractFromFileTest()
+        {
+            var file = Path.Join(outputPath, "a.html");
+            var html = File.ReadAllText(file);
+
+            string payload = GetPayloadFromWebpage(html);
+            var zone = ZoneFromJson(payload);
+        }
+
+        #endregion
+    }
+}
