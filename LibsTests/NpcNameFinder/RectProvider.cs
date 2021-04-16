ebb8058a8d1310da3bd17f5bb298af1bf0b5088a
Xian55 <polereczkrisz@gmail.com>
2021-04-17 01:52:54 +0200

    Test: Dummy RectProvider hints more 4:3 aspect ratio resolutions for npcnamefinder

--- LibsTests/NpcNameFinder/RectProvider.cs
+++ LibsTests/NpcNameFinder/RectProvider.cs
@@ -10,6 +10,8 @@ namespace LibsTests
         public void GetWindowRect(out Rectangle rect)
         {
             rect = new Rectangle(0, 0, 1920, 1080);
+            //rect = new Rectangle(0, 0, 3840, 2160);
+            //rect = new Rectangle(0, 0, 2560, 1440);
         }
 
         public Task RightClickMouse(Point position)
