b4f4397353c9b2a054d42d82e7be409b4f8ff127
Xian55 <polereczkrisz@gmail.com>
2021-04-25 14:25:31 +0200

    fix override existing addon files

--- BlazorServer/AddonConfigurator.cs
+++ BlazorServer/AddonConfigurator.cs
@@ -282,7 +282,7 @@ namespace BlazorServer
             foreach (FileInfo file in files)
             {
                 string tempPath = Path.Combine(destDirName, file.Name);
-                file.CopyTo(tempPath, false);
+                file.CopyTo(tempPath, true);
             }
 
             // If copying subdirectories, copy them and their contents to new location.
