b4a8f8d406695e60bccdda551f00e66896191337
Xian55 <polereczkrisz@gmail.com>
2021-04-25 04:03:29 +0200

    Introduce new project AddonConfig. BlazorServer uses it as a dep

---
+++ AddonConfig/AddonConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+public static class AddonConfigVersion
+{
+    public static int Version = 1;
+}
+
+public class AddonConfig
+{
+    public int Version = AddonConfigVersion.Version;
+
+    public string InstallPath;
+
+    public string GameVersion;
+    public string Author;
+    public string Title;
+
+    [NonSerialized]
+    public const string DefaultFileName = "addon_config.json";
+
+    public static AddonConfig Load()
+    {
+        if (File.Exists(DefaultFileName))
+        {
+            var loaded = JsonConvert.DeserializeObject<AddonConfig>(File.ReadAllText(DefaultFileName));
+            if (loaded.Version == AddonConfigVersion.Version)
+                return loaded;
+        }
+
+        return new AddonConfig().Save();
+    }
+
+    private AddonConfig Save()
+    {
+        File.WriteAllText(DefaultFileName, JsonConvert.SerializeObject(this));
+        return this;
+    }
+}
