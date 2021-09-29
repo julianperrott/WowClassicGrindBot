525d944b9665543a9e013b9dbe39b404c3212ae0
Xian55 <polereczkrisz@gmail.com>
2021-09-30 00:07:09 +0200

    TargetFinder: new compontent its only purpose to find a suitable enemy target based on the blacklist settings. it uses both mouse and tab targetting.

---
+++ Core/Goals/TargetFinder.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Core.Goals
+{
+    public class TargetFinder
+    {
+        private readonly ILogger logger;
+        private readonly ConfigurableInput input;
+        private readonly ClassConfiguration classConfig;
+        private readonly Wait wait;
+        private readonly PlayerReader playerReader;
+
+        private readonly IBlacklist blacklist;
+        private readonly NpcNameFinder npcNameFinder;
+
+        private readonly Random random = new Random();
+
+        public TargetFinder(ILogger logger, ConfigurableInput input, ClassConfiguration classConfig, Wait wait, PlayerReader playerReader, IBlacklist blacklist, NpcNameFinder npcNameFinder)
+        {
+            this.logger = logger;
+            this.classConfig = classConfig;
+            this.input = input;
+            this.wait = wait;
+            this.playerReader = playerReader;
+
+            this.blacklist = blacklist;
+            this.npcNameFinder = npcNameFinder;
+        }
+
+        public async Task<bool> Search(string source, CancellationToken cancellationToken)
+        {
+            if (!cancellationToken.IsCancellationRequested && !playerReader.PlayerBitValues.PlayerInCombat
+                && classConfig.TargetNearestTarget.MillisecondsSinceLastClick > random.Next(1000, 1500))
+            {
+                if (await LookForTarget(source, cancellationToken))
+                {
+                    if (playerReader.HasTarget && !playerReader.PlayerBitValues.TargetIsDead)
+                    {
+                        logger.LogInformation($"{source}: Has target!");
+                        return true;
+                    }
+                    else
+                    {
+                        if (!cancellationToken.IsCancellationRequested)
+                            await input.TapClearTarget($"{source}: Target is dead!");
+
+                        if (!cancellationToken.IsCancellationRequested)
+                            await wait.Update(1);
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        private async Task<bool> LookForTarget(string source, CancellationToken cancellationToken)
+        {
+            if (playerReader.HasTarget && !playerReader.PlayerBitValues.TargetIsDead && !blacklist.IsTargetBlacklisted())
+            {
+                return true;
+            }
+            else
+            {
+                if (!cancellationToken.IsCancellationRequested)
+                    await input.TapNearestTarget(source);
+
+                if (!playerReader.HasTarget && !cancellationToken.IsCancellationRequested)
+                {
+                    npcNameFinder.ChangeNpcType(NpcNameFinder.NPCType.Enemy);
+                    if (npcNameFinder.NpcCount > 0 && !cancellationToken.IsCancellationRequested)
+                    {
+                        await npcNameFinder.TargetingAndClickNpc(0, true, cancellationToken);
+
+                        if(!cancellationToken.IsCancellationRequested)
+                            await wait.Update(1);
+                    }
+                }
+            }
+
+            return !cancellationToken.IsCancellationRequested && playerReader.HasTarget && !blacklist.IsTargetBlacklisted();
+        }
+    }
+}
