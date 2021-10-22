6fb52123a460cebe6f07bae242061ce261508c34
Xian55 <polereczkrisz@gmail.com>
2021-10-22 16:58:48 +0200

    Added new Utility project ReadDBC_CSV_Spell

---
+++ Utilities/ReadDBC_CSV_Spell/Spell.cs
@@ -0,0 +1,15 @@
+﻿namespace ReadDBC_CSV_Spell
+{
+    public class Spell
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Level { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id} - {Name} - {Level}";
+        }
+    }
+}
