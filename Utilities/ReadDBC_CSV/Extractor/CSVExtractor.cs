bba99bd4fb4dc50e5291e87ef646621ab509035f
Xian55 <367101+Xian55@users.noreply.github.com>
2021-10-28 23:46:46 +0200

    Utility: add csv quote parsing

--- Utilities/ReadDBC_CSV/Extractor/CSVExtractor.cs
+++ Utilities/ReadDBC_CSV/Extractor/CSVExtractor.cs
@@ -40,5 +40,40 @@ namespace ReadDBC_CSV
             }
             throw new ArgumentOutOfRangeException(v);
         }
+
+        public string[] SplitQuotes(string csvText)
+        {
+            List<string> tokens = new List<string>();
+
+            int last = -1;
+            int current = 0;
+            bool inText = false;
+
+            while (current < csvText.Length)
+            {
+                switch (csvText[current])
+                {
+                    case '"':
+                        inText = !inText; break;
+                    case ',':
+                        if (!inText)
+                        {
+                            tokens.Add(csvText.Substring(last + 1, (current - last)).Trim(' ', ','));
+                            last = current;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                current++;
+            }
+
+            if (last != csvText.Length - 1)
+            {
+                tokens.Add(csvText.Substring(last + 1).Trim());
+            }
+
+            return tokens.ToArray();
+        }
     }
 }
